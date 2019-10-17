b2f742570aa44879de4a9ccdb774a572d15a278b
Aditya Soni <adityasoni5859@gmail.com>
2019-10-17 14:18:29 +0530

    added some more ui elements for tests and changed the test scenario

--- test_driver/e2e_test.dart
+++ test_driver/e2e_test.dart
@@ -13,8 +13,12 @@ void main() {
     // First, define the Finders and use them to locate widgets from the
     // test suite. Note: the Strings provided to the `byValueKey` method must
     // be the same as the Strings we used for the Keys in step 1.
-    final counterTextFinder = find.byValueKey('counter');
+    final counterTextFinder = find.byValueKey('counter_text');
     final buttonFinder = find.byTooltip('increment');
+    final buttonAdd = find.byValueKey('add');
+    final buttonSubtract = find.byValueKey('subtract');
+    final alertText = find.byValueKey('alert_text');
+    final buttonClose = find.byValueKey('close_button');
 
     FlutterDriver driver;
 
@@ -30,17 +34,17 @@ void main() {
       }
     });
 
-    test('starts at 0', () async {
-      // Use the `driver.getText` method to verify the counter starts at 0.
-      expect(await driver.getText(counterTextFinder), "0");
-    });
-
-    test('increments the counter', () async {
-      // First, tap the button.
+    test('Increment the counter', () async {
+      //Tap the floating action button
       await driver.tap(buttonFinder);
-
-      // Then, verify the counter text is incremented by 1.
+      //Verify that counter text has been increased by 1
       expect(await driver.getText(counterTextFinder), "1");
+      //
+      //tap on the button once more
+      await driver.tap(buttonFinder);
+      //Verify that counter text has been increased by 1 more
+      expect(await driver.getText(counterTextFinder), "2");
+      //
     });
   });
 }
